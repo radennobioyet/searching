e86142319dbcc7d8a6bb27c68b50e993dca0bc38
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-08 09:55:12 +0700

    binary

--- MahasiswaDemo19.java
+++ MahasiswaDemo19.java
@@ -35,5 +35,20 @@ public class MahasiswaDemo19 {
         int pss = (int) posisi;
         list.tampilPosisi(cari, pss);
         list.tampilDataSearch(cari, pss);
+
+        // melakukan pencarian data Binary
+        System.out.println("-------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        cari = sc.nextDouble();
+        System.out.println("-------------------------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("-------------------------------------------");
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs - 1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
     }
 }
